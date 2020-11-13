4310bfbe04759ccfb1ebf06058528ba916cb7146
Rebecca Weiss <rebeccascottweiss@gmail.com>
2020-11-12 17:02:37 -0800

    installed front end react app

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import io from "socket.io-client"; 
+
+function App() {
+  const [socket] = useState(() => io(':8000'));
+ 
+  useEffect(() => {
+    // we need to set up all of our event listeners
+    // in the useEffect callback function
+    console.log('Is this running?');
+    socket.on('Welcome', data => console.log(data));
+ 
+    // note that we're returning a callback function
+    // this ensures that the underlying socket will be closed if App is unmounted
+    // this would be more critical if we were creating the socket in a subcomponent
+    return () => socket.disconnect(true);
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
